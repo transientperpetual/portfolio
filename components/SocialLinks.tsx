ff8f602bdd7150f17ed4a5683b5f48ed2a996a66
skippersprint <iamjangidankit@gmail.com>
2025-06-17 08:51:12 +0530

    update : x removed

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -74,7 +74,7 @@ export default function SocialLinks() {
           />
         </svg>
       </a>
-
+{/* 
       <a
         href="https://x.com/jangidankit_"
         target="_blank"
@@ -91,7 +91,7 @@ export default function SocialLinks() {
             d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.28 21h2.65zM16.25 19L5.75 5h2l10.5 14z"
           />
         </svg>
-      </a>
+      </a> */}
     </div>
   );
 }
